95981e5acc3110a54c5cd08f74236f010fa3ffa4
UsmanJavaid008 <bcsf18m008@pucit.edu.pk>
2021-11-28 12:01:05 +0500

    First Dialog Button

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -22,4 +22,54 @@ public class MainActivity extends AppCompatActivity {
         builder.setTitle("Alert !");
         builder.show();
     }
+
+    public void showDialogTwoButton(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Do you want to exit?");
+        builder.setTitle("Exit");
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                "Yes",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int asdf) {
+                        finish();
+                    }
+                });
+        builder.setNegativeButton(
+                "No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    public void showDialogThreeButton(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Test Dialog")
+                .setMessage("Do you want to leave us")
+                .setPositiveButton("Leave", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("Stay", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNeutralButton("What up", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "Click Leave to close and Stay to cancel",
+                                Toast.LENGTH_LONG).show();
+                    }
+                }).show();
+    }
 }
