f6283a212feb72e3af353f88f2eff39ed86a2e19
UsmanJavaid008 <bcsf18m008@pucit.edu.pk>
2021-11-28 11:10:50 +0500

    First Dialog Button

--- app/src/main/java/com/example/alertdialog/MainActivity.java
+++ app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.alertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showAlertDialog(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("First Alert Dialog");
+        builder.setTitle("Alert !");
+        builder.show();
     }
 }
